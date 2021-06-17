25e656bc0ac5c68f510df3c563656b90678d2cc0
bradselcuk <bradselcuk@gmail.com>
2021-06-17 09:30:27 -0500

    as

--- DICTIONARY.py
+++ DICTIONARY.py
@@ -1,12 +1,34 @@
-urunler = {}
+urunler = {'1': {'ad': 'ford', 'fiyat': '5000'}, '2': {'ad': 'toyota', 'fiyat': '2000'}, '3': {'ad': 'Nissan', 'fiyat': '2500'}}
 
-id = input('id:  ')
-ad = input('ad:  ')
-fiyat = input('fiyat:  ')
 
-urunler[id] = {
-    "ad":ad,
-    "fiyat":fiyat
-}
+# id = input('id:  ')
+# ad = input('ad:  ')
+# fiyat = input('fiyat:  ')
+#
+# urunler[id] = {
+#     "ad":ad,
+#     "fiyat":fiyat
+# }
+# id = input('id:  ')
+# ad = input('ad:  ')
+# fiyat = input('fiyat:  ')
+#
+# urunler[id] = {
+#     "ad":ad,
+#     "fiyat":fiyat
+# }
+# id = input('id:  ')
+# ad = input('ad:  ')
+# fiyat = input('fiyat:  ')
+#
+# urunler[id] = {
+#     "ad":ad,
+#     "fiyat":fiyat
+# }
 
-print(urunler)
+id = input ('aradiginiz ID')
+urun = urunler[id]
+
+print(f'id: {id}, urun adi: {urun["ad"]},urun fiyati: {urun["fiyat"]}')
+# print(urunler)
+
