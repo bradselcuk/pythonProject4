bdf319df5cc15aa66d793cba6707758d3f19a54e
bradselcuk <bradselcuk@gmail.com>
2021-06-18 15:14:58 -0500

    fun

--- DAY2/For.py
+++ DAY2/For.py
@@ -7,3 +7,21 @@
 #         count = count + 1
 # print(count)
 # print(index)
+# for x in "banana":
+#   print(x)
+#
+#   fruits = ["apple", "banana", "cherry"]
+#   for x in fruits:
+#       print(x)
+#       if x == "banana":
+#           break
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+#
+# for x in adj:
+#   for y in fruits:
+#     print(x, y)
+
+# for x in [0, 1, 2]:
+#     pass
