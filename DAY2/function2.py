ee57595bd58b9e11a3579aaffa79ff6d2f671291
bradselcuk <bradselcuk@gmail.com>
2021-06-18 15:19:27 -0500

    funn

--- DAY2/function2.py
+++ DAY2/function2.py
@@ -1,6 +1,16 @@
-def my_function(fname):
-  print(fname + " Refsnes")
+# def my_function(fname):
+#   print(fname + " Refsnes")
+#
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
 
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus")
+def function2(number):
+    print(number*10)
+
+function2(20)
+
+def function3(number1,number2):
+    print(number1/number2)
+
+function3(100,2)
