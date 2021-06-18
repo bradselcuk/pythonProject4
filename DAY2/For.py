d73aebadf7541ede45c987d50e773795e358ae19
bradselcuk <bradselcuk@gmail.com>
2021-06-18 10:26:43 -0500

    defas

---
+++ DAY2/For.py
@@ -0,0 +1,9 @@
+# urunler = ['iphone 8','iphone 7','iphone X','iphone XR','samsung S10']
+#
+# count = 0
+# for urun in urunler:
+#     index = urun.find('n')
+#     if (index >-1):
+#         count = count + 1
+# print(count)
+# print(index)
---
+++ DAY2/karsilastirmaOpUygulama.py
@@ -0,0 +1,18 @@
+# sayi1 = int(input('ilk sayiyi giriniz:'))
+# sayi2 = int(input('ikinci sayiyi giriniz:'))
+# sonuc = (sayi1 > sayi2)
+# # print(sonuc)
+# print(f"{sayi1} {sayi2} den buyuktur: {sonuc}")
+
+sayi = int(input('sayiyi giriniz:'))  # Tek sayi mi?
+sonuc = (sayi % 2 == 0)
+print(sonuc)
+print(f"({sayi} cift sayidir {sonuc})")
+
+
+
+
+# hece = input('ilk hece:')
+# sonuc2 = (hece == 'haha')
+# print(sonuc2)
+
