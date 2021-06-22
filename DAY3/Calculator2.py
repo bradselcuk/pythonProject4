448487546d96eeba995cd4a97c0a6f7f2070b6cd
bradselcuk <bradselcuk@gmail.com>
2021-06-22 11:04:50 -0500

    prg2

--- DAY3/Calculator2.py
+++ DAY3/Calculator2.py
@@ -10,14 +10,34 @@ print("---------------------------------")
 if a == 1:
     print(" A D D I T I O N ")
     print("+++++++++++++++++")
+    num1 = int(input('Enter First Number: '))
+    num2 = int(input('Enter second Number: '))
+    def topla(num1,num2):
+        print(f"Result: {num1+num2}")
+    topla(num1,num2)
+# --------------------------------------------
 elif a == 2:
      print(" S U B T R A C T I O N ")
      print("_________________")
+     num3 = int(input('Enter First Number: '))
+     num4 = int(input('Enter second Number: '))
+     def cikart(num3,num4):
+         print(f"Result: {num3 - num4}")
+     cikart(num3,num4)
+
 elif a == 3:
      print(" M U L T I P L I C A T I O N ")
      print("*****************")
+     num5 = int(input('Enter First Number: '))
+     num6 = int(input('Enter second Number: '))
+     def carp(num5, num6):
+         print(f"Result: {num5 * num6}")
+     carp(num5, num6)
 elif a == 4:
      print("D I V I S I O N ")
      print("////////////////")
-else:
-    print("Application has been ended")
+     num7 = int(input('Enter First Number: '))
+     num8 = int(input('Enter second Number: '))
+     def bol(num7, num8):
+         print(f"Result: {num7 / num8}")
+     bol(num7, num8)
---
+++ DAY4/Practice1.py
@@ -0,0 +1,33 @@
+# 1- Kendisine gonderilen bir kelimeyi belirtilen kez ekranda yazan fonksiyon yaz
+
+# def yazdir(txt,adet):
+#     print(txt * adet)
+#
+# yazdir("Merhaba - ", 5)
+
+# 2- Dikdortgenin alan ve cevresini hesaplayan fonksiyonu yaziniz
+
+# def hesapla(kisa, uzun):
+#     alan = kisa * uzun
+#     cevre = 2 * (kisa+uzun)
+#
+#     print (f"alan: {alan}  cevre: {cevre}")
+#
+# hesapla(5,7)
+
+# 3 - yazi tura uygulamasinin fonksiyonunu yaz
+import random
+
+
+# def yaziTuraAt():
+#     import random
+#     sayi = random.random()
+#
+#     if sayi>0.5:
+#         return "Tura"
+#     else:
+#         return "Yazi"
+#
+# print(yaziTuraAt())
+
+
