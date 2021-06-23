c31f8a67229039011556028df08dc7df8f503cea
bradselcuk <bradselcuk@gmail.com>
2021-06-23 16:43:54 -0500

    atm

---
+++ DAY5/ATM.py
@@ -0,0 +1,32 @@
+#ATM APP
+
+account1 = {
+    'name': 'Brad',
+    'accountNumber': '12345678',
+    'balance': 3000,
+    'overdraft': 2000
+}
+account2 = {
+    'name': 'Kate',
+    'accountNumber': '32145678',
+    'balance': 3000,
+    'overdraft': 2000
+}
+
+def withdraw(account, amount):
+    print(f"Welcome {account['name']}")
+
+    if account['balance'] >= amount:
+        print('You can take your money')
+    else:
+        total = account['balance']+account['overdraft']
+        if total >= amount:
+            useoverdraft = input('Do you want use overdarft?  y/n')
+            if useoverdraft == 'y':
+                print('You can get money')
+            if useoverdraft == 'n':
+                print(f"{account['accountNumber']} is available just {account['balance']}")
+        else:
+            print("You don't have enough balance in your account")
+
+withdraw(account2, 15000)
