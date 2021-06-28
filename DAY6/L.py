190f273b88cbfdbeccd65d226de2facb2c0ec31b
bradselcuk <bradselcuk@gmail.com>
2021-06-28 10:16:50 -0500

    lll

---
+++ DAY6/L.py
@@ -0,0 +1,14 @@
+cars = ["Ford", "Volvo", "BMW", "Audi"]    # List  Add,remove,loop
+print(cars)
+
+cars2 = ["Ford", "Volvo", "BMW", "Audi"]   # Array
+count=0
+for x in cars2:
+    count= count+1
+    print(x)
+
+cars3 = {"Ford", "Volvo", "BMW", "Audi"}    # Set no Add
+print(cars3)
+
+cars4 = ("Ford", "Volvo", "BMW", "Audi")   # Tuples  noAdd
+print(cars4)
--- DAY6/List_Array_ArrayList.py
+++ DAY6/List_Array_ArrayList.py
@@ -17,12 +17,11 @@
 
 
 a = input("Enter new car: ")
-cars = ["Ford", "Volvo", "BMW"]
-cars.append(a)
+cars = ["Ford", "Volvo", "BMW", "Audi"]
 cars.sort()
+cars.append(a)
 
 count=0
 for x in cars:
     count= count+1
     print(f" line:  {count} ===> {x}")
-
