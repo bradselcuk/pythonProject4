04130d9cf2bf35b8bb9c145ecd7c8fcb926e0079
bradselcuk <bradselcuk@gmail.com>
2021-07-06 16:18:30 -0500

    cla

--- DAY9/CLASS.py
+++ DAY9/CLASS.py
@@ -6,13 +6,27 @@ class Personel:
         self.lastname = lastname.title()
         self.salary = salary
         self.email = f'{firstname.lower()}_{lastname.lower()}@company.com'
+    def full_name(self):
+        return f'{self.firstname} {self.lastname}'
+
 
 per_1 = Personel('John', 'Smith', 30000)
 per_2 = Personel('Mary', 'Smith', 60000)
 
-print(per_1.firstname)
-print(per_1.lastname)
-print(per_1.email)
-print(per_1.salary)
+print(per_1.full_name())
+# print(per_2.full_name())
 
-print(per_2.firstname, per_2.lastname, per_2.email, per_2.salary)
+print(Personel.full_name(per_2))
+
+
+#
+# print(per_1.firstname)
+# print(per_1.lastname)
+# print(per_1.email)
+# print(per_1.salary)
+#
+# print(per_2.firstname, per_2.lastname, per_2.email, per_2.salary)
+#
+# print(f'{per_1.firstname} {per_1.lastname}')
+#
+#
